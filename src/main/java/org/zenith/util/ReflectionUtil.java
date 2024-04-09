21f9212fbcd948dc9358aa9f45551de7847e3e75
Dennis Moes <dennismoes@me.com>
2024-04-09 10:15:42 +0200

    Added a class to read all the models using reflection

---
+++ src/main/java/org/zenith/util/ReflectionUtil.java
@@ -0,0 +1,62 @@
+package org.zenith.util;
+
+import org.zenith.annotation.Entity;
+
+import java.io.File;
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+
+public class ReflectionUtil {
+    /**
+     * Attempt to load the class by the classname
+     *
+     * @param classname The name of the class to be processed.
+     * @return The Class object corresponding to the given classname, or null if the class is not found.
+     */
+    private Class<?> processClass(String classname) {
+        try {
+            return Class.forName("org.zenith.model." + classname.replace(".java", ""));
+        } catch (ClassNotFoundException ex) {
+            ex.printStackTrace();
+            return null;
+        }
+    }
+
+    /**
+     * Retrieves a list of database model class annotated with the {@link Entity} annotation.
+     *
+     * @return A list of Class objects representing database model classes.
+     */
+    public List<Class<?>> getDbModels() {
+        List<Class<?>> classes = new ArrayList<>();
+
+        String path = System.getProperty("user.dir") + "/src/main/java/org/zenith/model";
+        File directory = new File(path);
+
+        if (directory.exists() && directory.isDirectory()) {
+            for (File file : Objects.requireNonNull(directory.listFiles())) {
+                Class<?> classObj = processClass(file.getName());
+
+                // Make sure that the model has the Entity annotation
+                if (classObj != null && classObj.isAnnotationPresent(Entity.class)) {
+                    classes.add(classObj);
+                }
+            }
+        }
+
+        return classes;
+    }
+
+    /**
+     * Retrieves the fields of a given model class.
+     *
+     * @param classObj The class object representing the model class.
+     * @return A list of {@link Field} objects representing the fields of the model class.
+     */
+    public List<Field> getFieldsOfModel(Class<?> classObj) {
+        return Arrays.stream(classObj.getFields()).toList();
+    }
+}
