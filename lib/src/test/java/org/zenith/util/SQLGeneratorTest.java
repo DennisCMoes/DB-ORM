38877ba4333911fbc2497260d70cd693e191a46b
Dennis Moes <dennismoes@me.com>
2025-01-25 08:33:12 +0100

    refactor: group similar tests
    Used the @Nested annotation to group the GenerateDelete, GenerateUpdate, GenerateSelect, and GenerateInsert functions together

--- lib/src/test/java/org/zenith/util/SQLGeneratorTest.java
+++ lib/src/test/java/org/zenith/util/SQLGeneratorTest.java
@@ -1,5 +1,6 @@
 package org.zenith.util;
 
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.zenith.annotation.Column;
 import org.zenith.annotation.Id;
@@ -103,227 +104,239 @@ class SQLGeneratorTest {
         assertThrows(IllegalArgumentException.class, () -> SQLGenerator.generateDropTable(classes));
     }
 
-    @Test
-    void shouldGenerateValidInsertWithNullFields() throws IllegalArgumentException, NoSuchFieldException, IllegalAccessException {
-        TestModel1 model = new TestModel1();
-        model.id = 1;
-        model.name = null;
+    @Nested
+    class GenerateInsert {
+        @Test
+        void shouldGenerateValidInsertWithNullFields() throws IllegalArgumentException, NoSuchFieldException, IllegalAccessException {
+            TestModel1 model = new TestModel1();
+            model.id = 1;
+            model.name = null;
 
-        String result = SQLGenerator.generateInsert(model);
-        String expected = """
+            String result = SQLGenerator.generateInsert(model);
+            String expected = """
                 INSERT INTO testmodel1 (id, name) VALUES (1, NULL) RETURNING *;""";
 
-        assertEquals(expected, result);
-    }
+            assertEquals(expected, result);
+        }
 
-    @Test
-    void shouldGenerateValidInsertWithNullOneToOneRelationship() throws IllegalArgumentException, NoSuchFieldException, IllegalAccessException {
-        TestModel1 model = new TestModel1();
-        model.id = 1;
-        model.name = null;
+        @Test
+        void shouldGenerateValidInsertWithNullOneToOneRelationship() throws IllegalArgumentException, NoSuchFieldException, IllegalAccessException {
+            TestModel1 model = new TestModel1();
+            model.id = 1;
+            model.name = null;
 
-        String result = SQLGenerator.generateInsert(model);
-        String expected = """
+            String result = SQLGenerator.generateInsert(model);
+            String expected = """
                 INSERT INTO testmodel1 (id, name) VALUES (1, NULL) RETURNING *;""";
 
-        assertEquals(expected, result);
-    }
+            assertEquals(expected, result);
+        }
 
-    @Test
-    void shouldGenerateValidInsertForTestClass1() throws IllegalArgumentException, NoSuchFieldException, IllegalAccessException {
-        TestModel1 model = new TestModel1();
-        model.id = 1;
-        model.name = "Hello";
+        @Test
+        void shouldGenerateValidInsertForTestClass1() throws IllegalArgumentException, NoSuchFieldException, IllegalAccessException {
+            TestModel1 model = new TestModel1();
+            model.id = 1;
+            model.name = "Hello";
 
-        String result = SQLGenerator.generateInsert(model);
-        String expected = """
+            String result = SQLGenerator.generateInsert(model);
+            String expected = """
                 INSERT INTO testmodel1 (id, name) VALUES (1, 'Hello') RETURNING *;""";
 
-        assertEquals(expected, result);
-    }
+            assertEquals(expected, result);
+        }
 
-    @Test
-    void shouldGenerateValidInsertWithEmptyString() throws IllegalArgumentException, NoSuchFieldException, IllegalAccessException {
-        TestModel1 model = new TestModel1();
-        model.id = 1;
-        model.name = "";
+        @Test
+        void shouldGenerateValidInsertWithEmptyString() throws IllegalArgumentException, NoSuchFieldException, IllegalAccessException {
+            TestModel1 model = new TestModel1();
+            model.id = 1;
+            model.name = "";
 
-        String result = SQLGenerator.generateInsert(model);
-        String expected = """
+            String result = SQLGenerator.generateInsert(model);
+            String expected = """
                 INSERT INTO testmodel1 (id, name) VALUES (1, '') RETURNING *;""";
 
-        assertEquals(expected, result);
-    }
+            assertEquals(expected, result);
+        }
 
-    @Test
-    void shouldGenerateValidInsertForModelWIthAllNullFields() throws IllegalArgumentException, NoSuchFieldException, IllegalAccessException {
-        TestModel1 model = new TestModel1();
+        @Test
+        void shouldGenerateValidInsertForModelWIthAllNullFields() throws IllegalArgumentException, NoSuchFieldException, IllegalAccessException {
+            TestModel1 model = new TestModel1();
 
-        String result = SQLGenerator.generateInsert(model);
-        String expected = """
+            String result = SQLGenerator.generateInsert(model);
+            String expected = """
                 INSERT INTO testmodel1 (id, name) VALUES (0, NULL) RETURNING *;""";
 
-        assertEquals(expected, result);
-    }
+            assertEquals(expected, result);
+        }
 
-    @Test
-    void shouldGenerateValidInsertForTestClass1WithOneToOne() throws IllegalArgumentException, NoSuchFieldException, IllegalAccessException {
-        TestModel1 model = new TestModel1();
-        model.id = 1;
-        model.name = "Hello";
+        @Test
+        void shouldGenerateValidInsertForTestClass1WithOneToOne() throws IllegalArgumentException, NoSuchFieldException, IllegalAccessException {
+            TestModel1 model = new TestModel1();
+            model.id = 1;
+            model.name = "Hello";
 
-        TestModel2 model2 = new TestModel2();
-        model2.age = 10;
-        model2.salary = 10;
-        model2.parent = model;
+            TestModel2 model2 = new TestModel2();
+            model2.age = 10;
+            model2.salary = 10;
+            model2.parent = model;
 
-        String result = SQLGenerator.generateInsert(model2);
-        String expected = """
+            String result = SQLGenerator.generateInsert(model2);
+            String expected = """
                 INSERT INTO testmodel2 (id, age, salary, isWorking, parent_id) VALUES (0, 10, '10.0', false, 1) RETURNING *;""";
 
-        assertEquals(expected, result);
-    }
+            assertEquals(expected, result);
+        }
 
-    @Test
-    void shouldGenerateValidInsertForTestClass2() throws IllegalArgumentException, NoSuchFieldException, IllegalAccessException {
-        TestModel2 model = new TestModel2();
-        model.age = 10;
-        model.salary = 12.0;
+        @Test
+        void shouldGenerateValidInsertForTestClass2() throws IllegalArgumentException, NoSuchFieldException, IllegalAccessException {
+            TestModel2 model = new TestModel2();
+            model.age = 10;
+            model.salary = 12.0;
 
-        String result = SQLGenerator.generateInsert(model);
-        String expected = """
+            String result = SQLGenerator.generateInsert(model);
+            String expected = """
                 INSERT INTO testmodel2 (id, age, salary, isWorking, parent_id) VALUES (0, 10, '12.0', false, NULL) RETURNING *;""";
 
-        assertEquals(expected, result);
+            assertEquals(expected, result);
+        }
+
+        @Test
+        void shouldThrowIllegalArgumentExceptionWhenGeneratingInsertWithNoAnnotatedFields() {
+            assertThrows(IllegalArgumentException.class, () -> SQLGenerator.generateInsert(new EmptyModel()));
+        }
     }
 
-    @Test
-    void shouldThrowIllegalArgumentExceptionWhenGeneratingInsertWithNoAnnotatedFields() {
-        assertThrows(IllegalArgumentException.class, () -> SQLGenerator.generateInsert(new EmptyModel()));
+    @Nested
+    class GenerateSelect {
+        @Test
+        void shouldGenerateSelectAllColumnsWithoutFilters() throws NoSuchFieldException {
+            String result = SQLGenerator.generateSelect(TestModel1.class, null, null);
+            String expected = "SELECT * FROM testmodel1;";
+
+            assertEquals(expected, result);
+        }
+
+        @Test
+        void shouldGenerateSelectSpecificColumnsWithoutFilters() throws NoSuchFieldException {
+            List<String> fieldsToReturn = List.of("id", "name");
+
+            String result = SQLGenerator.generateSelect(TestModel1.class, fieldsToReturn, null);
+            String expected = "SELECT id, name FROM testmodel1;";
+
+            assertEquals(expected, result);
+        }
+
+        @Test
+        void shouldGenerateSelectAllColumnsWithSingleFilter() throws NoSuchFieldException {
+            Map<String, Object> fieldsToQuery = Map.of("id", 1);
+
+            String result = SQLGenerator.generateSelect(TestModel1.class, null, fieldsToQuery);
+            String expected = "SELECT * FROM testmodel1 WHERE id=1;";
+
+            assertEquals(expected, result);
+        }
+
+        @Test
+        void shouldGenerateSelectSpecificColumnsWithMultipleFilters() throws NoSuchFieldException {
+            List<String> fieldsToReturn = List.of("id", "name");
+            Map<String, Object> fieldsToQuery = Map.of("id", 1, "name", "Hello");
+
+            String result = SQLGenerator.generateSelect(TestModel1.class, fieldsToReturn, fieldsToQuery);
+            String expected = "SELECT id, name FROM testmodel1 WHERE name='Hello' AND id=1;";
+
+            assertEquals(expected, result);
+        }
+
+        @Test
+        void shouldGenerateSelectAllColumnsWithNullFieldValue() throws NoSuchFieldException {
+            Map<String, Object> fieldsToQuery = new HashMap<>();
+            fieldsToQuery.put("name", null);
+            fieldsToQuery.put("id", 1);
+
+            String result = SQLGenerator.generateSelect(TestModel1.class, null, fieldsToQuery);
+            String expected = "SELECT * FROM testmodel1 WHERE name='null' AND id=1;";
+
+            assertEquals(expected, result);
+        }
+
+        @Test
+        void shouldGenerateSelectAllColumnsWithNoFilters() throws NoSuchFieldException {
+            String result = SQLGenerator.generateSelect(TestModel1.class, null, new HashMap<>());
+            String expected = "SELECT * FROM testmodel1;";
+
+            assertEquals(expected, result);
+        }
+
+        @Test
+        void shouldThrowExceptionWhenFieldAnnotationsAreMissing() {
+            assertThrows(NullPointerException.class, () -> {
+                SQLGenerator.generateSelect(EmptyModel.class, List.of("id"), Map.of("id", null));
+            });
+        }
     }
 
-    @Test
-    void shouldGenerateSelectAllColumnsWithoutFilters() throws NoSuchFieldException {
-        String result = SQLGenerator.generateSelect(TestModel1.class, null, null);
-        String expected = "SELECT * FROM testmodel1;";
+    @Nested
+    class GenerateUpdate {
+        @Test
+        void shouldGenerateUpdateQueryForSimpleFields() throws NoSuchFieldException, IllegalAccessException {
+            TestModel1 model = new TestModel1();
+            model.id = 1;
+            model.name = "UpdatedName";
 
-        assertEquals(expected, result);
+            String result = SQLGenerator.generateUpdate(model);
+            String expected = "UPDATE testmodel1 SET name='UpdatedName' WHERE id=1 RETURNING *;";
+
+            assertEquals(expected, result);
+        }
+
+        @Test
+        void shouldGenerateUpdateQueryWithNullValues() throws NoSuchFieldException, IllegalAccessException {
+            TestModel1 model = new TestModel1();
+            model.id = 2;
+            model.name = null;
+
+            String result = SQLGenerator.generateUpdate(model);
+            String expected = "UPDATE testmodel1 SET name='null' WHERE id=2 RETURNING *;";
+
+            assertEquals(expected, result);
+        }
+
+        @Test
+        void shouldGenerateUpdateQueryWithRelationshipFields() throws NoSuchFieldException, IllegalAccessException {
+            TestModel1 model1 = new TestModel1();
+            model1.id = 1;
+            model1.name = "John Doe";
+
+            TestModel2 model2 = new TestModel2();
+            model2.id = 1;
+            model2.age = 10;
+            model2.parent = model1;
+            model2.isWorking = true;
+
+            String result = SQLGenerator.generateUpdate(model2);
+            String expected = "UPDATE testmodel2 SET age=10, salary='0.0', isWorking=true, parent_id=1 WHERE id=1 RETURNING *;";
+
+            assertEquals(expected, result);
+        }
+
+        @Test
+        void shouldThrowExceptionForNoFieldsToUpdate() {
+            EmptyModel model = new EmptyModel();
+            assertThrows(NoSuchFieldException.class, () -> SQLGenerator.generateUpdate(model));
+        }
     }
 
-    @Test
-    void shouldGenerateSelectSpecificColumnsWithoutFilters() throws NoSuchFieldException {
-        List<String> fieldsToReturn = List.of("id", "name");
+    @Nested
+    class GenerateDelete {
+        @Test
+        void shouldGenerateDeleteQueryForValidModel() throws NoSuchFieldException, IllegalAccessException {
+            TestModel1 model = new TestModel1();
+            model.id = 1;
 
-        String result = SQLGenerator.generateSelect(TestModel1.class, fieldsToReturn, null);
-        String expected = "SELECT id, name FROM testmodel1;";
+            String result = SQLGenerator.generateDelete(model);
+            String expected = "DELETE FROM testmodel1 WHERE id=1 RETURNING *;";
 
-        assertEquals(expected, result);
-    }
-
-    @Test
-    void shouldGenerateSelectAllColumnsWithSingleFilter() throws NoSuchFieldException {
-        Map<String, Object> fieldsToQuery = Map.of("id", 1);
-
-        String result = SQLGenerator.generateSelect(TestModel1.class, null, fieldsToQuery);
-        String expected = "SELECT * FROM testmodel1 WHERE id=1;";
-
-        assertEquals(expected, result);
-    }
-
-    @Test
-    void shouldGenerateSelectSpecificColumnsWithMultipleFilters() throws NoSuchFieldException {
-        List<String> fieldsToReturn = List.of("id", "name");
-        Map<String, Object> fieldsToQuery = Map.of("id", 1, "name", "Hello");
-
-        String result = SQLGenerator.generateSelect(TestModel1.class, fieldsToReturn, fieldsToQuery);
-        String expected = "SELECT id, name FROM testmodel1 WHERE name='Hello' AND id=1;";
-
-        assertEquals(expected, result);
-    }
-
-    @Test
-    void shouldGenerateSelectAllColumnsWithNullFieldValue() throws NoSuchFieldException {
-        Map<String, Object> fieldsToQuery = new HashMap<>();
-        fieldsToQuery.put("name", null);
-        fieldsToQuery.put("id", 1);
-
-        String result = SQLGenerator.generateSelect(TestModel1.class, null, fieldsToQuery);
-        String expected = "SELECT * FROM testmodel1 WHERE name='null' AND id=1;";
-
-        assertEquals(expected, result);
-    }
-
-    @Test
-    void shouldGenerateSelectAllColumnsWithNoFilters() throws NoSuchFieldException {
-        String result = SQLGenerator.generateSelect(TestModel1.class, null, new HashMap<>());
-        String expected = "SELECT * FROM testmodel1;";
-
-        assertEquals(expected, result);
-    }
-
-    @Test
-    void shouldThrowExceptionWhenFieldAnnotationsAreMissing() {
-        assertThrows(NullPointerException.class, () -> {
-            SQLGenerator.generateSelect(EmptyModel.class, List.of("id"), Map.of("id", null));
-        });
-    }
-
-    @Test
-    void shouldGenerateUpdateQueryForSimpleFields() throws NoSuchFieldException, IllegalAccessException {
-        TestModel1 model = new TestModel1();
-        model.id = 1;
-        model.name = "UpdatedName";
-
-        String result = SQLGenerator.generateUpdate(model);
-        String expected = "UPDATE testmodel1 SET name='UpdatedName' WHERE id=1 RETURNING *;";
-
-        assertEquals(expected, result);
-    }
-
-    @Test
-    void shouldGenerateUpdateQueryWithNullValues() throws NoSuchFieldException, IllegalAccessException {
-        TestModel1 model = new TestModel1();
-        model.id = 2;
-        model.name = null;
-
-        String result = SQLGenerator.generateUpdate(model);
-        String expected = "UPDATE testmodel1 SET name='null' WHERE id=2 RETURNING *;";
-
-        assertEquals(expected, result);
-    }
-
-    @Test
-    void shouldGenerateUpdateQueryWithRelationshipFields() throws NoSuchFieldException, IllegalAccessException {
-        TestModel1 model1 = new TestModel1();
-        model1.id = 1;
-        model1.name = "John Doe";
-
-        TestModel2 model2 = new TestModel2();
-        model2.id = 1;
-        model2.age = 10;
-        model2.parent = model1;
-        model2.isWorking = true;
-
-        String result = SQLGenerator.generateUpdate(model2);
-        String expected = "UPDATE testmodel2 SET age=10, salary='0.0', isWorking=true, parent_id=1 WHERE id=1 RETURNING *;";
-
-        assertEquals(expected, result);
-    }
-
-    @Test
-    void shouldThrowExceptionForNoFieldsToUpdate() {
-        EmptyModel model = new EmptyModel();
-        assertThrows(NoSuchFieldException.class, () -> SQLGenerator.generateUpdate(model));
-    }
-
-    @Test
-    void shouldGenerateDeleteQueryForValidModel() throws NoSuchFieldException, IllegalAccessException {
-        TestModel1 model = new TestModel1();
-        model.id = 1;
-
-        String result = SQLGenerator.generateDelete(model);
-        String expected = "DELETE FROM testmodel1 WHERE id=1 RETURNING *;";
-
-        assertEquals(expected, result);
+            assertEquals(expected, result);
+        }
     }
 }
