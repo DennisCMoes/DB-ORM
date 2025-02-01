2503035a07b8c71bdff444e24f300954a6a75f56
Dennis Moes <dennismoes@me.com>
2025-02-01 09:55:53 +0100

    refactor: small improvements
    Rewrote some logics in the code to make it more stable and just improve the overall code

--- lib/src/main/java/org/zenith/util/ReflectionUtil.java
+++ lib/src/main/java/org/zenith/util/ReflectionUtil.java
@@ -1,17 +1,13 @@
 package org.zenith.util;
 
 import org.zenith.annotation.Column;
-import org.zenith.annotation.Entity;
 import org.zenith.annotation.Id;
-import org.zenith.annotation.relation.ManyToMany;
 import org.zenith.annotation.relation.ManyToOne;
 import org.zenith.annotation.relation.OneToMany;
 import org.zenith.annotation.relation.OneToOne;
-import org.zenith.enumeration.ColumnType;
 import org.zenith.model.interfaces.IModel;
 import org.zenith.util.reflection.*;
 
-import java.io.File;
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
@@ -19,31 +15,18 @@ import java.lang.reflect.ParameterizedType;
 import java.lang.reflect.Type;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.text.SimpleDateFormat;
 import java.util.*;
 
 public class ReflectionUtil {
-    private final static Map<Class<? extends Annotation>, FieldMappingStrategy> MAPPING_STRATEGIES = Map.of(
+    private static final Set<Class<? extends Annotation>> RELATION_ANNOTATIONS = Set.of(OneToOne.class, ManyToOne.class);
+    private static final Map<Class<? extends Annotation>, FieldMappingStrategy> MAPPING_STRATEGIES = Map.of(
             Id.class, new IdFieldMappingStrategy(),
             ManyToOne.class, new ManyToOneMappingStrategy(),
             OneToMany.class, new OneToManyFieldMappingStrategy(),
             Column.class, new ColumnFieldMappingStrategy());
 
-    /**
-     * Attempt to load the class by the classname
-     *
-     * @param classname The name of the class to be processed.
-     * @return The Class object corresponding to the given classname, or null if the class is not found.
-     */
-    private Class<? extends IModel> processClass(String classname) {
-        try {
-            @SuppressWarnings("unchecked")
-            Class<? extends IModel> model = (Class<? extends IModel>) Class.forName("org.zenith.model." + classname.replace(".java", ""));
-
-            return model;
-        } catch (ClassNotFoundException ex) {
-            return null;
-        }
+    private ReflectionUtil() {
+        throw new UnsupportedOperationException("This is an utility class and cannot be instantiated");
     }
 
     /**
@@ -62,7 +45,6 @@ public class ReflectionUtil {
             throws SQLException, IllegalAccessException, InstantiationException, NoSuchMethodException, InvocationTargetException, NoSuchFieldException {
 
         T model = modelClass.getDeclaredConstructor().newInstance();
-        String modelName = model.getClass().getSimpleName();
         Field[] fields = modelClass.getDeclaredFields();
 
         for (Field field : fields) {
@@ -75,6 +57,9 @@ public class ReflectionUtil {
 
             field.setAccessible(true);
             Annotation[] annotations = field.getDeclaredAnnotations();
+
+            if (annotations.length == 0)
+                continue;
 
             for (Annotation annotation : annotations) {
                 FieldMappingStrategy strategy = MAPPING_STRATEGIES.get(annotation.annotationType());
@@ -89,40 +74,13 @@ public class ReflectionUtil {
     }
 
     /**
-     * Retrieves a list of database model class annotated with the {@link Entity} annotation.
-     *
-     * @return A list of Class objects representing database model classes.
-     */
-    public List<Class<? extends IModel>> getDbModels() {
-        List<Class<? extends IModel>> classes = new ArrayList<>();
-
-        String path = System.getProperty("user.dir") + "/lib/src/main/java/org/zenith/model";
-        File directory = new File(path);
-
-        if (directory.exists() && directory.isDirectory()) {
-            for (File file : Objects.requireNonNull(directory.listFiles())) {
-                if (file.isDirectory()) continue;
-
-                Class<? extends IModel> classObj = processClass(file.getName());
-
-                // Make sure that the model has the Entity annotation
-                if (classObj != null && classObj.isAnnotationPresent(Entity.class)) {
-                    classes.add(classObj);
-                }
-            }
-        }
-
-        return classes;
-    }
-
-    /**
      * Retrieves the fields of a given model class.
      *
      * @param model The class object representing the model class.
      * @return A list of {@link Field} objects representing the fields of the model class.
      */
-    public List<Field> getFieldsOfModel(Class<? extends IModel> model) {
-        return Arrays.stream(model.getFields()).toList();
+    public static List<Field> getFieldsOfModel(Class<? extends IModel> model) {
+        return List.of(model.getDeclaredFields());
     }
 
     /**
@@ -132,7 +90,7 @@ public class ReflectionUtil {
      * @param annotations A list of annotations to ignore.
      * @return A list of {@link Field} objects representing the fields of the model class.
      */
-    public List<Field> getFieldsOfModelWithoutTypes(Class<? extends IModel> model, List<Class<? extends Annotation>> annotations) {
+    public static List<Field> getFieldsOfModelWithoutTypes(Class<? extends IModel> model, List<Class<? extends Annotation>> annotations) {
         return getFieldsOfModel(model)
                 .stream()
                 .filter(field -> annotations.stream().noneMatch(field::isAnnotationPresent))
@@ -146,7 +104,7 @@ public class ReflectionUtil {
      * @param annotations A list of annotations to ignore.
      * @return A list of {@link Field} objects representing the fields of the model class.
      */
-    public List<Field> getFieldsOfModelWithTypes(Class<? extends IModel> model, List<Class<? extends Annotation>> annotations) {
+    public static List<Field> getFieldsOfModelWithTypes(Class<? extends IModel> model, List<Class<? extends Annotation>> annotations) {
         return getFieldsOfModel(model)
                 .stream()
                 .filter(field -> annotations.stream().anyMatch(field::isAnnotationPresent))
@@ -162,8 +120,10 @@ public class ReflectionUtil {
      * @throws NoSuchFieldException If the field with the specified name is not found in the model
      * @throws IllegalAccessException if the field cannot be accessed (e.g., if it's private)
      */
-    public Object getValueOfField(IModel model, String fieldName) throws NoSuchFieldException, IllegalAccessException {
-        return getFieldByName(model.getClass(), fieldName).get(model);
+    public static Object getValueOfField(IModel model, String fieldName) throws NoSuchFieldException, IllegalAccessException {
+        Field field = getFieldByName(model.getClass(), fieldName);
+        field.trySetAccessible();
+        return field.get(model);
     }
 
     /**
@@ -174,8 +134,10 @@ public class ReflectionUtil {
      * @return The {@link Field} object corresponding to the specified name
      * @throws NoSuchFieldException If a field with the specified name is not found in the class
      */
-    public Field getFieldByName(Class<? extends IModel> model, String fieldName) throws NoSuchFieldException {
-        return model.getDeclaredField(fieldName);
+    public static Field getFieldByName(Class<? extends IModel> model, String fieldName) throws NoSuchFieldException {
+        Field field = model.getDeclaredField(fieldName);
+        field.trySetAccessible();
+        return field;
     }
 
     /**
@@ -184,29 +146,26 @@ public class ReflectionUtil {
      * @param field The {@link Field} for which the database column name s to be determined
      * @return The database column name as a {@link String}
      */
-    public String getFieldName(Field field) {
-        String fieldName = field.getName();
-
-        if (field.isAnnotationPresent(OneToOne.class) || field.isAnnotationPresent(ManyToOne.class)) {
-            fieldName += "_id";
-        }
-
-        return fieldName;
+    public static String getFieldName(Field field) {
+        return RELATION_ANNOTATIONS
+                .stream()
+                .anyMatch(field::isAnnotationPresent)
+                    ? field.getName() + "_id"
+                    : field.getName();
     }
 
-    public Class<?> getFieldType(Class<?> modelClass, String fieldName) throws NoSuchFieldException {
+    public static Class<?> getFieldType(Class<?> modelClass, String fieldName) throws NoSuchFieldException {
         Field field = modelClass.getDeclaredField(fieldName);
         field.setAccessible(true);
 
         if (List.class.isAssignableFrom(field.getType())) {
             Type genericType = field.getGenericType();
 
-            if (genericType instanceof ParameterizedType) {
-                ParameterizedType parameterizedType = (ParameterizedType) genericType;
+            if (genericType instanceof ParameterizedType parameterizedType) {
                 Type[] typeArguments = parameterizedType.getActualTypeArguments();
 
-                if (typeArguments.length > 0) {
-                    return (Class<?>) typeArguments[0];
+                if (typeArguments.length > 0 && typeArguments[0] instanceof Class<?> classObj) {
+                    return classObj;
                 }
             }
         }
